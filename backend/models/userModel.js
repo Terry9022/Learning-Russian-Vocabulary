a553ad635f35d9a8d7fb3041304604f5d29b5114
Terry9022 <karta1020376@gmail.com>
2024-03-12 21:37:14 -0400

    add vocabularySchema in userSchema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,28 @@
 import mongoose from "mongoose";
 
+const vocabularySchema = new mongoose.Schema({
+  russian_word: {
+    type: String,
+    required: true,
+  },
+  english_word: {
+    type: String,
+    required: true,
+  },
+  part_of_speech: {
+    type: String,
+    required: true,
+  },
+  example_sentence: {
+    type: String,
+    required: true,
+  },
+  difficulty_level: {
+    type: String,
+    required: true,
+  },
+});
+
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -15,18 +38,18 @@ const userSchema = mongoose.Schema(
       require: true,
     },
     level: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     vocabulary_received: [
-        {
-            vocabulary: vocabularySchema,
-            timestamp: {
-                type: Date,
-                default: Date.now,
-            }
-        }
-    ]
+      {
+        vocabulary: vocabularySchema,
+        timestamp: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
   },
   {
     timestamps: true,
