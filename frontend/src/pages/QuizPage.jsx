b6dd2f071715e525823dda150f6e216a335954e2
mwampner <wampnm@rpi.edu>
2024-04-08 11:26:50 -0400

    Reroute to signin if quiz or review selected and no user signed in

--- frontend/src/pages/QuizPage.jsx
+++ frontend/src/pages/QuizPage.jsx
@@ -1,16 +1,59 @@
 import { FaPen } from "react-icons/fa";
 import Header from "../components/Header";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const QuizPage = () => {
+  const [vocabularies, setVocabularies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [level, setLevel] = useState([]);
+  const [user, setUser] = useState([]);
+  const [name, setName] = useState([]);
+  const [email, setEmail] = useState([]);
+
+  const navigateTo = useNavigate();
+  const handleSignOut = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    navigateTo("/");
+  };
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const userId = localStorage.getItem("userId");
+
+        const response = await axios.get(
+          `http://localhost:5555/api/user/${userId}`
+        );
+        const userData = response.data;
+        console.log(userData);
+        setUser(userData);
+        setName(userData.name);
+        setEmail(userData.email);
+        setLevel(userData.level);
+        setVocabularies(userData.vocabulary_received);
+      } catch (error) {
+        console.error(error);
+        alert("Error fetching user profile");
+      }
+    };
+
+  fetchUserProfile();
+}, []);
+
   return (
     <div>
       <div className="body">
         <div>
         <nav>
               <Link to="/">Home</Link>
-              <Link to="/quiz">Quiz</Link>
-              <Link to="/review">Review</Link>
+              {!user && <Link to="/signin">Quiz</Link>}
+              {!user && <Link to="/signin">Review</Link>}
               {!user && <Link to="/signin">Sign in</Link>}
+              {user && <Link to="/quiz">Quiz</Link>}
+              {user && <Link to="/review">Review</Link>}
               {user && <Link to="/profile">Profile</Link>}
               {user && <Link onClick={handleSignOut}>Sign out</Link>}
           </nav>
